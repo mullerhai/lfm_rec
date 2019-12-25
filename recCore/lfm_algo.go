a560ab9f65c4e10a4a59bc2b3dfb69145057a9e8
muller <hai710459649@foxmail.com>
2019-12-25 12:14:12 +0800

    add dict mapping

--- recCore/lfm_algo.go
+++ recCore/lfm_algo.go
@@ -28,31 +28,42 @@ func main() {
 	}
 }
 
+type IdsMapping struct {
+	userIdIndexDict map[float64]float64
+	indexUserIdDict map[float64]float64
+	itemIdIndexDict map[float64]float64
+	indexItemIdDict map[float64]float64
+}
 type LFM struct {
-	class_count        int
-	iter_count         int
-	featureName        string
-	labelName          string
-	lr                 float64
-	lam                float64
-	raw_data           *mat.Dense
-	modelPfactor       *mat.Dense
-	modelQfactor       *mat.Dense
-	ratingDf           *dataframe.DataFrame
-	useridItemidDict map[float64]map[float64]float64
-	useridSet  *gset.Set
-	itemidSet  *gset.Set
+	class_count            int
+	iter_count             int
+	featureName            string
+	labelName              string
+	lr                     float64
+	lam                    float64
+	raw_data               *mat.Dense
+	modelPfactor           *mat.Dense
+	modelQfactor           *mat.Dense
+	ratingDf               *dataframe.DataFrame
+	useridItemidDict       map[float64]map[float64]float64
+	useridSet              *gset.Set
+	itemidSet              *gset.Set
+	idsMapping             *IdsMapping
+	userIndexItemIndexDict map[float64]map[float64]float64
 }
 
 func NewDefaultLFM(ratingPath string) *LFM {
 	denseArr, ratingDf := loadData(ratingPath)
 	useridItemidDict := make(map[float64]map[float64]float64)
-	lfm := &LFM{5, 5, "userId", "itemId", 0.02, 0.01, denseArr, nil, nil, ratingDf, useridItemidDict,nil,nil}
+	userIndexItemIndexDict := make(map[float64]map[float64]float64)
+	lfm := &LFM{5, 5, "userId", "itemId", 0.02, 0.01, denseArr, nil, nil, ratingDf, useridItemidDict, nil, nil, nil, userIndexItemIndexDict}
 	return lfm
 }
 
-func (lfm *LFM)generateUserIdItemIdDict(){
+func (lfm *LFM) generateUserIdItemIdIndexDict() {
 	ratingDf := lfm.ratingDf
+	useridIndexDict := lfm.idsMapping.userIdIndexDict
+	itemidIndexDict := lfm.idsMapping.itemIdIndexDict
 	iterator := ratingDf.Values(dataframe.ValuesOptions{0, 1, true}) // Don't apply read lock because we are write locking from outside.
 	ratingDf.Lock()
 	for {
@@ -60,100 +71,127 @@ func (lfm *LFM)generateUserIdItemIdDict(){
 		if row == nil {
 			break
 		}
-		if _,ok := lfm.useridItemidDict[vals["UserID"].(float64)];ok{
-			userMap := lfm.useridItemidDict[vals["UserID"].(float64)]
-			if _,ok := userMap[vals["MovieID"].(float64)];ok{
-				ratingVal := userMap[vals["MovieID"].(float64)]
-				newRatingVal :=ratingVal+vals["Rating"].(float64)
-				userMap[vals["MovieID"].(float64)] =newRatingVal
-			}else{
-				userMap[vals["MovieID"].(float64)] =vals["Rating"].(float64)
+		userId, movieId, rating := vals["UserID"].(float64), vals["MovieID"].(float64), vals["Rating"].(float64)
+		userIndex, movieIndex := float64(useridIndexDict[userId]), float64(itemidIndexDict[movieId])
+		fmt.Println("userid movieid ,rating ", userId, movieId, rating)
+		fmt.Println("userIndex  movieIndex ", userIndex, movieIndex)
+		if _, ok := lfm.useridItemidDict[userId]; ok {
+			userMap := lfm.useridItemidDict[userId]
+			userIndexMap := lfm.userIndexItemIndexDict[userIndex]
+			if _, ok := userMap[movieId]; ok {
+				ratingVal := userMap[movieId]
+				newRatingVal := ratingVal + rating
+				userMap[movieId] = newRatingVal
+			} else {
+				userMap[movieId] = rating
 			}
-			lfm.useridItemidDict[vals["UserID"].(float64)] =userMap
-		}else{
-			movieRatingDict := map[float64]float64{vals["MovieID"].(float64):vals["Rating"].(float64)}
-			lfm.useridItemidDict[vals["UserID"].(float64)] =movieRatingDict
+			if _, ok := userIndexMap[movieIndex]; ok {
+				ratingVals := userIndexMap[movieIndex]
+				newRatingVals := ratingVals + rating
+				userIndexMap[movieIndex] = newRatingVals
+			} else {
+				userIndexMap[movieIndex] = rating
+			}
+			lfm.useridItemidDict[userId] = userMap
+			lfm.userIndexItemIndexDict[userIndex] = userIndexMap
+		} else {
+			movieRatingDict := map[float64]float64{movieId: rating}
+			lfm.useridItemidDict[userId] = movieRatingDict
+			movieIndexRatingDict := map[float64]float64{movieIndex: rating}
+			lfm.userIndexItemIndexDict[userIndex] = movieIndexRatingDict
 		}
-
-		//usermap := lfm.useridItemidDict[vals["UserID"].(float64)]
 		//fmt.Println(*row, vals)
-
 	}
 	ratingDf.Unlock()
-	fmt.Println("lfm dict len ",len(lfm.useridItemidDict))
+	fmt.Println("lfm dict len ", len(lfm.useridItemidDict))
 
 }
 func loadData(ratingPath string) (*mat.Dense, *dataframe.DataFrame) {
 	var ctx = context.Background()
 	file, err := os.Open(ratingPath)
-	//strings.NewReader(ratingPath)
-	//var csvOp imports.CSVLoadOptions
-	//
-	//csvOp.DictateDataType =make(map[string]interface{})
-	//csvOp.DictateDataType["UserID"]= float64(0)
-	//csvOp.DictateDataType["MovieID"]=float64(0)
-	//csvOp.DictateDataType["Rating"]=float64(0)
-	//csvOp.DictateDataType["Timestamp"]=float64(0)
-	//csvOp.Comma =','
-
 	csvOp := imports.CSVLoadOptions{
-		Comma: ',',
-		Comment:0,
+		Comma:   ',',
+		Comment: 0,
 		DictateDataType: map[string]interface{}{
-			"UserID": float64(0),
-			"MovieID": float64(0),
-			"Rating": float64(0),
+			"UserID":    float64(0),
+			"MovieID":   float64(0),
+			"Rating":    float64(0),
 			"Timestamp": float64(0),
 		},
 	}
 	fmt.Println(csvOp)
-	ratingDf, err := imports.LoadFromCSV(ctx, file,csvOp)
+	ratingDf, err := imports.LoadFromCSV(ctx, file, csvOp)
 	if err != nil {
-		fmt.Println("load error ",err.Error())
+		fmt.Println("load error ", err.Error())
 	}
 	//fmt.Println(ratingDf)
-
 	float64Arr := seriesConvertFloatarray(ratingDf)
-	matrix_row := ratingDf.Series[0].NRows()
-	matrix_col := len(ratingDf.Names())
-	dense_arr := mat.NewDense(matrix_row, matrix_col, float64Arr)
-	return dense_arr, ratingDf
+	matrixRow := ratingDf.Series[0].NRows()
+	matrixCol := len(ratingDf.Names())
+	denseArr := mat.NewDense(matrixRow, matrixCol, float64Arr)
+	return denseArr, ratingDf
 }
 
 func (lfm *LFM) initModel() {
-
 	src := rand.New(rand.NewSource(1))
-	var uni =distuv.Uniform{0,0.35,src}
+	var uni = distuv.Uniform{0, 0.35, src}
 	pDistinctEle := distinctSeriesConvertSet(lfm.ratingDf.Series[0])
 	qDistinctEle := distinctSeriesConvertSet(lfm.ratingDf.Series[1])
 	pClasslen := lfm.class_count * pDistinctEle.Size()
 	qClasslen := lfm.class_count * qDistinctEle.Size()
-	pData := GenerateUniformArr(uni,int64(pClasslen))
-	qData := GenerateUniformArr(uni,int64(qClasslen))
+	pData := GenerateUniformArr(uni, int64(pClasslen))
+	qData := GenerateUniformArr(uni, int64(qClasslen))
 	lfm.itemidSet = &qDistinctEle
 	lfm.useridSet = &pDistinctEle
-	lfm.modelPfactor = mat.NewDense(pDistinctEle.Size(),lfm.class_count,pData)
-	lfm.modelQfactor = mat.NewDense(qDistinctEle.Size(),lfm.class_count,qData)
-	lfm.generateUserIdItemIdDict()
+	lfm.modelPfactor = mat.NewDense(pDistinctEle.Size(), lfm.class_count, pData)
+	lfm.modelQfactor = mat.NewDense(qDistinctEle.Size(), lfm.class_count, qData)
+	lfm.generateIdsMatrixIndexMapping()
+	lfm.generateUserIdItemIdIndexDict()
+
 }
 
-func distinctSeriesConvertSet(series dataframe.Series) gset.Set{
-	fseries := series.(*dataframe.SeriesFloat64).Values
+func (lfm *LFM) generateIdsMatrixIndexMapping() {
+	var userIdArr = make([]float64, 0)
+	var itemIdArr = make([]float64, 0)
+	lfm.useridSet.Iterator(func(v interface{}) bool {
+		userIdArr = append(userIdArr, v.(float64))
+		return true
+	})
+	lfm.itemidSet.Iterator(func(v interface{}) bool {
+		itemIdArr = append(itemIdArr, v.(float64))
+		return true
+	})
+	var UserIdIndexDict = make(map[float64]float64)
+	var IndexUserIdDict = make(map[float64]float64)
+	var ItemIdIndexDict = make(map[float64]float64)
+	var indexItemIdDict = make(map[float64]float64)
+	for index, uid := range userIdArr {
+		UserIdIndexDict[uid] = float64(index)
+		IndexUserIdDict[float64(index)] = uid
+	}
+	for index, itemId := range itemIdArr {
+		ItemIdIndexDict[itemId] = float64(index)
+		indexItemIdDict[float64(index)] = itemId
+	}
+	idMapping := &IdsMapping{UserIdIndexDict, IndexUserIdDict, ItemIdIndexDict, indexItemIdDict}
+	lfm.idsMapping = idMapping
+}
+func distinctSeriesConvertSet(series dataframe.Series) gset.Set {
+	seriesArr := series.(*dataframe.SeriesFloat64).Values
 	var newSet = gset.New(true)
-	for _,ele :=range fseries{
+	for _, ele := range seriesArr {
 		newSet.Add(ele)
 	}
 	return *newSet
 }
 
-
 func (lfm *LFM) _preidct(userId, itemId float64) float64 {
-	p := lfm.modelPfactor.RowView(int(userId)).(*mat.VecDense) //5,1
+	p := lfm.modelPfactor.RowView(int(userId)).(*mat.VecDense)     //5,1
 	q := lfm.modelQfactor.RowView(int(itemId)).(*mat.VecDense).T() // 1.5
 	var res mat.Dense
 	res.Mul(p, q) //5,5
 	res_r, res_c := res.Caps()
-	fmt.Println("predict res shape ",res_r,res_c)
+	fmt.Println("predict res shape ", res_r, res_c)
 	r := mat.Sum(&res)
 	logit := 1.0 / (1 + math.Exp(-r))
 	return logit
@@ -166,23 +204,23 @@ func (lfm *LFM) _loss(user_id, item_id, y float64, step int) float64 {
 }
 
 func (lfm *LFM) _optimize(user_id, item_id, e float64) {
-	modp_r,modp_c :=lfm.modelPfactor.Caps()
-	fmt.Println("modelPfactor shape  ",modp_r,modp_c)
-	modq_r,modq_c := lfm.modelQfactor.Caps()
-	fmt.Println("modelqfactor sh ape  ",modq_r,modq_c)
+	modp_r, modp_c := lfm.modelPfactor.Caps()
+	fmt.Println("modelPfactor shape  ", modp_r, modp_c)
+	modq_r, modq_c := lfm.modelQfactor.Caps()
+	fmt.Println("modelqfactor sh ape  ", modq_r, modq_c)
 	pVal := lfm.modelPfactor.RowView(int(user_id)).(*mat.VecDense)
 	qVal := lfm.modelQfactor.RowView(int(item_id)).(*mat.VecDense)
 	var gradient_p, l2_p, grad_l2_p, delta_p, gradient_q, l2_q, grad_l2_q, delta_q, np_val, nq_val mat.VecDense
 	gradient_p.ScaleVec(-e, qVal)
-	g_r,g_c := gradient_p.Caps()
-	p_r,p_c := pVal.Caps()
-	q_r,q_c := qVal.Caps()
-	fmt.Println("gradient_p shape  ",g_r,g_c)
-	fmt.Println("pval shape ",p_r,p_c)
-	fmt.Println("qval shape ",q_r,q_c)
+	g_r, g_c := gradient_p.Caps()
+	p_r, p_c := pVal.Caps()
+	q_r, q_c := qVal.Caps()
+	fmt.Println("gradient_p shape  ", g_r, g_c)
+	fmt.Println("pval shape ", p_r, p_c)
+	fmt.Println("qval shape ", q_r, q_c)
 	l2_p.ScaleVec(lfm.lam, pVal)
-	l2_p_r,l2_p_c := l2_p.Caps() //shape 5,1
-	fmt.Println("l2_p shape ",l2_p_r,l2_p_c)
+	l2_p_r, l2_p_c := l2_p.Caps() //shape 5,1
+	fmt.Println("l2_p shape ", l2_p_r, l2_p_c)
 	grad_l2_p.AddVec(&gradient_p, &l2_p)
 	delta_p.ScaleVec(lfm.lr, &grad_l2_p)
 	gradient_q.ScaleVec(-e, pVal)
@@ -206,16 +244,16 @@ func vectofloat(vec mat.Vector) []float64 {
 	return vecArr
 }
 func (lfm *LFM) Train() {
-	fmt.Println("lfm training ",lfm.iter_count)
+	fmt.Println("lfm training ", lfm.iter_count)
 	for step := 0; step < lfm.iter_count; step++ {
-		fmt.Println(" lfm step ",step)
-		for userId, valDict := range lfm.useridItemidDict {
-			itemIds := reflect.ValueOf(valDict).MapKeys()
-			fmt.Println("itemids len : ,userid ",len(itemIds),userId)
-			for itemId := range itemIds {
-				cost_err := lfm._loss(userId, float64(itemId), valDict[float64(itemId)], step)
-				lfm._optimize(userId, float64(itemId), cost_err)
-				fmt.Println("step  costerror ",step,cost_err)
+		fmt.Println(" lfm step ", step)
+		for userIdIndex, valDict := range lfm.userIndexItemIndexDict {
+			itemIdIndexs := reflect.ValueOf(valDict).MapKeys()
+			fmt.Println("itemids len : ,userid ", len(itemIdIndexs), userIdIndex)
+			for itemId := range itemIdIndexs {
+				costErr := lfm._loss(userIdIndex, float64(itemId), valDict[float64(itemId)], step)
+				lfm._optimize(userIdIndex, float64(itemId), costErr)
+				fmt.Println("step  costerror ", step, costErr)
 			}
 		}
 		lfm.lr *= 0.9
@@ -294,16 +332,13 @@ func seriesConvertFloatarray(df *dataframe.DataFrame) []float64 {
 	return arr
 }
 
-func GenerateUniformArr(uni distuv.Uniform,arrLen int64)[]float64 {
+func GenerateUniformArr(uni distuv.Uniform, arrLen int64) []float64 {
 	x := make([]float64, arrLen)
 	generateSample(x, uni)
 	sort.Float64s(x)
 	fmt.Println(x)
 	return x
 }
-//type Rander interface {
-//	Rand() float64
-//}
 func generateSample(x []float64, r distuv.Rander) {
 	for i := range x {
 		x[i] = r.Rand()
